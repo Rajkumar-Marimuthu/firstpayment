36a9fc740e873ac138d431475b0ee9d48cbe003a
rajmaritech <rajmaritech@gmail.com>
2022-02-14 09:03:51 +0530

    add subdirectories

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "time"
+
+// card
+// user
+type User struct {
+	firstName   string
+	middleName  string
+	lastName    string
+	cardDetails card
+}
+
+type card struct {
+	cardNum      string
+	cardName     string
+	cvv          int
+	expiryDate   time.Time
+	cardProvider string
+	issuerBank   string
+}
+
 func main() {
 
 }
